4d06a13ef420667fda4ee5b1e87717dcfbd829b3
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-10-26 03:21:02 +0000

    Create joint_gabbling.rb

---
+++ joint_gabbling.rb
@@ -0,0 +1,39 @@
+#require "NeoPathfinding"
+
+gabbler = Gabbler.new                     # Create new Gabbler instance.
+
+a = gabbler.learn("[ Who ] Marie Antionette [ What ] Is guillotined in the public square. [ When ] On October 16, 1793. [ Where ] At the scaffold in the city of Paris. [ Why ] Because she said let them eat cake, when the French people needed Bread. [ How ] An angular and sharp eighty pound blade fell on her neck, taking her head right off.")
+
+b = gabbler.learn("[ Who ] Ann Boleyn [ What ] Is beheaded by sword. [ When ] 19 May 1536 [ Where ] On a private scaffold within the castle. [ Why ] She was framed for committing treason against the crown. [ How ] With a sword brandished by a French excutioner imported from Paris.")
+
+c = gabbler.learn("[ Who ] American Civil War [ What ] Is fought between the confederates and union. [ When ] Between 1861 to 1864. [ Where ] In various states split along the Mason-Dixon Line. [ Why ] The simple history is that the South wanted to split from the Union do to slavery. [ How ] Using canons, early trench warfare, and an evolution of early Submarines.")
+
+2.times do
+  get_statistics(:cette,    a[0],
+                 :maisette, a[1],
+                 :sinon,    a[2])
+                 
+                 dynamic_reward_allocation
+
+  get_statistics(:cette,    b[0],
+                 :maisette, b[1],
+                 :sinon,    b[2])
+                 
+                 dynamic_reward_allocation
+
+  get_statistics(:cette,    c[0],
+                 :maisette, c[1],
+                 :sinon,    c[2])
+                 
+                 dynamic_reward_allocation
+
+  2.times do
+    get_statistics(:cette,          cette("Marie Antionette", "le mort"),
+                   :maisette,            maisette("Napoleon", "le mort"),
+                   :sinon,    sinon("Anna Boleyn", "le vie", "le drole"))
+
+    dynamic_reward_allocation
+  end
+end
+
+gabbler.unlearn!                          # Forget Sherlock Holmes.
